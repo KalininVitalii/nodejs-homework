8568e37948863d6a30b9b799290a4c917c9da85f
KalininVitalii <105376147+KalininVitalii@users.noreply.github.com>
2023-01-02 19:31:52 +0200

    doing

---
+++ controllers/auth.js
@@ -0,0 +1,88 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const { User } = require("../models/user");
+
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const { SECRET_KEY } = process.env;
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+
+  res.status(201).json({
+    name: newUser.name,
+    email: newUser.email,
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password invalid");
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password invalid");
+  }
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
+
+  res.json({
+    token,
+  });
+};
+
+const getCurrent = (req, res) => {
+  const { name, email } = req.user;
+
+  res.json({
+    name,
+    email,
+  });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+  res.json({
+    message: "Logout success",
+  });
+};
+
+const subscriptionChange = async (req, res) => {
+  const { subscription } = req.body;
+  const { _id } = req.user;
+  const user = await User.findByIdAndUpdate(_id, { subscription });
+  if (!user) {
+    throw HttpError(401, "Not authorized");
+  }
+  res.json({
+    message: `Subscription successfully changed on ${subscription}`,
+  });
+};
+
+module.exports = {
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+  subscriptionChange: ctrlWrapper(subscriptionChange),
+};
